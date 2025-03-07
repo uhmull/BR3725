e5640f294f8737916ca0fbbe9c6de8161f63ee00
uhmull <146273318+uhmull@users.noreply.github.com>
2025-03-07 15:15:32 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,14 +1,14 @@
-# this gets you to 16 rather then 15 (I tried my best to learn implement shifting but this is a new concept for me)
+#This gets to 16 not 15 because I just learned what this is but I can explain how I got to 16
 
 #What I Learned Today: 
 #We can use "<<" in python for shifting left and ">>" for shifting right.
 #We can use shifting with binary numbers. We can convert regular number to binary.
 #Left shiffting will multtiply it by two each time you shift it 
-# I am not completly sure but based on this I would guess that a right shift will divide by 2??
-# its very unlikely we will se bitwise operations as its more theory 
+# right shift will divide by 2
+# its very unlikely we will see bitwise operations often as its more theory 
 
-def multiply(n):
-    n = 1
-    n = n + 1
-    return n << 3
-print(multiply(5))
+def multiply(n): #i created a procedure
+    n = 1 #i set n equal to 1
+    n = n + 1 #i incremented n by 1
+    return n << 3 # this means that we multiply it by 2 three times because we left shift it three times and each left shift you multiply by 2
+print(multiply(5)) #i am printing the final result
